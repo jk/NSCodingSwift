da227c3e706c8ab22dd1d18c9d1c006ac2341669
jk <jens.kohl@gmail.com>
2016-01-29 16:23:01 +0100

    Initial Commit

---
+++ NSCodingProtocol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSCodingProtocol
+//
+//  Created by Jens Kohl on 29.01.16.
+//  Copyright © 2016 Jens Kohl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
